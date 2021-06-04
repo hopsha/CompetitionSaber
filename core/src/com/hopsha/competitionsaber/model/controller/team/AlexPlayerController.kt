c9a47519cff8335f73c7869f6495757e755d0a50
aleksandr_alekseevich <alexpers92@gmail.com>
2021-06-04 18:52:07 +0300

    Super Alex algorithm

--- core/src/com/hopsha/competitionsaber/model/controller/team/AlexPlayerController.kt
+++ core/src/com/hopsha/competitionsaber/model/controller/team/AlexPlayerController.kt
@@ -1,5 +1,67 @@
 package com.hopsha.competitionsaber.model.controller.team
 
-import com.hopsha.competitionsaber.model.controller.AutoPlayerController
+import com.hopsha.competitionsaber.degrees
+import com.hopsha.competitionsaber.model.Engine
+import com.hopsha.competitionsaber.model.controller.*
+import com.hopsha.competitionsaber.model.entity.Saber
+import kotlin.random.Random
 
-class AlexPlayerController : AutoPlayerController()
+class AlexPlayerController : PlayerController{
+
+    private val isLeftAligned = Random.nextBoolean()
+
+    override suspend fun decide(vision: Vision, input: Engine.Input, state: PlayerState): Action {
+        val closestPlayer = vision.findClosestPlayer()
+        val largestFreeRange = vision.freeRanges
+                .maxByOrNull { it.width }
+        val closestItem = vision.items.minByOrNull { it.distance }
+        val closestDistance = closestItem?.distance ?: Float.MAX_VALUE
+
+        return when {
+            vision.items.isEmpty() -> Action.MOVE_BACKWARD
+            shouldRandomlyTurnLeft() -> Action.TURN_LEFT
+            shouldRandomlyTurnRight() -> Action.TURN_RIGHT
+            largestFreeRange == null -> turnAlignedSide()
+
+            closestPlayer != null -> {
+                val closestPlayerCenter = closestPlayer.angleRange.center
+                when {
+                    !state.isAttacking && closestPlayer.distance < Saber.LENGTH -> Action.ATTACK
+                    closestPlayer.angleRange.contains(0.degrees) -> Action.MOVE_FORWARD
+                    closestPlayerCenter.degrees < 0 -> Action.TURN_RIGHT
+                    closestPlayerCenter.degrees > 0 -> Action.TURN_LEFT
+                    else -> Action.MOVE_BACKWARD
+                }
+            }
+            largestFreeRange.width.degrees < 88 -> turnAlignedSide()
+            largestFreeRange.center.degrees > 10 -> Action.TURN_LEFT
+            largestFreeRange.center.degrees < -10 -> Action.TURN_RIGHT
+            else -> Action.MOVE_BACKWARD//Action.MOVE_FORWARD
+        }
+    }
+
+    private fun shouldRandomlyTurnLeft(): Boolean {
+        return false
+//        if (isLeftAligned && Random.nextInt() % 8 == 0) {
+//
+//            true
+//        } else !isLeftAligned && Random.nextInt() % 10 == 0
+    }
+
+    private fun shouldRandomlyTurnRight(): Boolean {
+        return false
+    }
+
+    private fun Vision.findClosestPlayer(): Vision.Item? {
+        return items.asSequence()
+                .filter { it.info is Vision.Info.Player }
+                .minByOrNull { it.distance }
+    }
+
+    private fun turnAlignedSide(): Action = if (isLeftAligned) {
+        Action.TURN_LEFT
+    } else {
+        Action.TURN_RIGHT
+    }
+
+}
